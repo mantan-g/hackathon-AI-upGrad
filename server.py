c31a1d25aa9c41af40a4ae72af565a22ab710f97
Shubham Prakash <shubhamprakash702@gmail.com>
2025-09-07 00:09:56 +0530

    Added endpoint for generating a preview of a program

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, Depends
 from .services import ask_llm
 from db import get_db
 from services.db_service import DBService
+from services.llm_service import LLMService
 
-app = FastAPI(title="LangChain + FastAPI + MongoDB")
+app = FastAPI(title="Program Preview Generator")
 db = DBService()
 
-@app.post("/ask")
-async def ask_question():
-    db.find_one("")
-    return {"a": "Hello WOrld"}
+@app.post("/generate_preview")
+async def ask_question(pregram_id: str):
+    llm_service = LLMService()
+    output = llm_service.generate_preview_for_engaging_modules(pregram_id)
+    return output
